26a28fc902ef7049cf7f1af28a659597daad54ae
Vadim Vrublevsky <v.v.vrublevsky@gmail.com>
2019-10-31 07:56:25 +0300

    [feat] small rework

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ gulp.task("style", function() {
     .pipe(gulp.dest("source/css"))
     .pipe(gulp.dest("build/css"))
     .pipe(minify())
-    // .pipe(rename("style.min.css"))
+    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("source/css"))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
